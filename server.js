b738b5004bb3ffbb47bd6d09c725f006b26df507
Matteo Niccoli <matteo@chorally.com>
2020-08-22 15:23:24 +0000

    * Some fixes, added standardjs formatting dependencies. * Removed socket-client dependencies replated directly by a cdn link   on term.html. * Changed some colour.

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const spawn = require('child_process').spawn;
-const sh = spawn('bash');
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const spawn = require('child_process').spawn
+const sh = spawn('bash')
 
 app.use(express.static('./'))
 
-sh.stdout.on('data', function(data) {
-  io.emit('message', data);
-});
+sh.stdout.on('data', function (data) {
+  io.emit('message', data)
+})
 
-sh.stderr.on('data', function(data) {
-  io.emit('message', data);
-});
+sh.stderr.on('data', function (data) {
+  io.emit('message', data)
+})
 
 sh.on('exit', function (code) {
-  io.emit('exit', '** Shell exited: '+code+' **');
-});
+  io.emit('exit', '** Shell exited: ' + code + ' **')
+})
 
-io.on('connection', function(client){
-  client.on('message', function(data){
-    sh.stdin.write(data+"\n");
-    io.emit('message', new Buffer("> "+data));
-  });
-});
+io.on('connection', function (client) {
+  client.on('message', function (data) {
+    sh.stdin.write(data + '\n')
+    io.emit('message', new Buffer('> ' + data))
+  })
+})
 
-server.listen(8080, function(){
-  console.log('server started');
+server.listen(8090, function () {
+  console.log('server started')
 })
